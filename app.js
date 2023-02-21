aabc93f6430100bb264c8f293fbe30d09ce214e3
gary909 <garywhite61@hotmail.com>
2023-02-21 12:40:09 +0100

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function cookie(x){
+    // ...
+}
+
+console.log(cookie("Ryan")); // "Who ate the last cookie? It was Zach!"
+console.log(cookie(26)); // "Who ate the last cookie? It was Monica!"
+console.log(cookie(2.3)); // "Who ate the last cookie? It was Monica!"
+console.log(cookie(true)); // "Who ate the last cookie? It was the dog!!"
