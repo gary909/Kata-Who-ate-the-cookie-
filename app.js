921b3481b958ef16cee7af43b5c40437a72bd662
gary909 <garywhite61@hotmail.com>
2023-02-21 12:44:42 +0100

    code working

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 function cookie(x){
-    // ...
+    if (typeof x === 'string'){
+        return "Who ate the last cookie? It was Zach!";
+    } else if (typeof x === 'number'){
+        return "Who ate the last cookie? It was Monica!";
+    } else {
+        return "Who ate the last cookie? It was the dog!";
+    }
 }
 
 console.log(cookie("Ryan")); // "Who ate the last cookie? It was Zach!"
 console.log(cookie(26)); // "Who ate the last cookie? It was Monica!"
 console.log(cookie(2.3)); // "Who ate the last cookie? It was Monica!"
-console.log(cookie(true)); // "Who ate the last cookie? It was the dog!!"
+console.log(cookie(true)); // "Who ate the last cookie? It was the dog!"
